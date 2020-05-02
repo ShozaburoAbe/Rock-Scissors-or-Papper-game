b5178a1c19b91ded989f1a4d6021c377e8f997ce
阿部祥三郎 <shozaburoabe@Shozaburos-MacBook-Pro.local>
2020-05-02 15:12:35 +0900

    add functions and start game

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const startGameBtn = document.getElementById('start-game-btn');
+// assgin them for prevent typo
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEAFULT_USER_CHOICE = ROCK;
 
-function startGame() {
+gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(`${ROCK}, ${PAPER} or ${PAPER}`, "").toUpperCase(); 
+  if (
+    selection !== ROCK && 
+    selection !== PAPER && 
+    selection !== SCISSORS
+    ) {
+      alert(`Invaild choice! We chose ${DEAFULT_USER_CHOICE} for you!`);
+      return DEAFULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", () => {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
   console.log("Game is starting...");
-}
-
-startGameBtn.addEventListener("click", startGame);
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
+  
+});
